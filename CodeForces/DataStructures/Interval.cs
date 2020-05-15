0c6bd079a9e2be76b7d664d10251e36dcf6ca075
Gregory Levit <levitg@gmail.com>
2020-05-16 01:01:57 +0300

    Added Helpers and DataStructures(Heap)

---
+++ CodeForces/DataStructures/Interval.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace CodeForces.DataStructures
+{
+    public class Interval
+    {
+        public int l;
+        public int r;
+        public int len;
+    }
+
+    public class IntervalComparer : Comparer<Interval>
+    {
+        public override int Compare(Interval x, Interval y)
+        {
+            if (x.len > y.len)
+            {
+                return 1;
+            }
+            if (x.len < y.len)
+            {
+                return -1;
+            }
+
+            if (x.l < y.l)
+            {
+                return 1;
+            }
+
+            if (x.l > y.l)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ CodeForces/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeForces
+{
+    class Helpers
+    {
+        // 4 1 0 4 
+        static int[] LineToArray(string line)
+        {
+            return (from v in line.Split(' ') select int.Parse(v)).ToArray();
+        }
+
+
+        static string PrintList(IList<int> list)
+        {
+            var sb = new StringBuilder();
+            foreach (int a in list)
+            {
+                sb.Append(a);
+                sb.Append(" ");
+            }
+            return sb.ToString();
+        }
+    }
+}
